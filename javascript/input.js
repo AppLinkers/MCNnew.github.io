e23a4733943e618a9be143b66d06a37e68dcaf40
rio319-an <78468001+rio319-an@users.noreply.github.com>
2022-10-06 16:54:00 +0900

    contact

---
+++ javascript/input.js
@@ -0,0 +1,42 @@
+const optionMenu = document.querySelector(".select-menu"),
+    selectBtn = optionMenu.querySelector(".select-btn"),
+    options = optionMenu.querySelectorAll(".option"),
+    sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+const role = document.getElementById("role");
+
+const trader_name = document.querySelector(".trader__name");
+
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
+
+options.forEach(option => {
+    option.addEventListener("click", () => {
+        let selectedOption = option.querySelector(".option-text").innerText;
+        role.value = selectedOption;
+        optionMenu.classList.remove("active");
+        trader_name.innerText = selectedOption;
+    })
+})
+
+
+// contact에서만
+const show = document.getElementById("show");
+const non_show = document.getElementById("non-show");
+const show_password = document.querySelector(".show-password");
+const allow = document.querySelector(".form-allow");
+
+show.addEventListener("click", () => {
+    show.classList.add("active");
+    non_show.classList.remove("active");
+    show_password.classList.remove("active");
+})
+
+non_show.addEventListener("click", () => {
+    non_show.classList.add("active");
+    show.classList.remove("active");
+    show_password.classList.add("active");
+})
+
+allow.addEventListener("click", () => {
+    allow.classList.toggle("active");
+})
