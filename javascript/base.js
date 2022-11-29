5f5d1ffb378e4f63d133f749e117f341fba0613c
rio319-an <78468001+rio319-an@users.noreply.github.com>
2022-11-29 15:19:10 +0900

    11/29 fix ui

--- javascript/base.js
+++ javascript/base.js
@@ -1,11 +1,4 @@
-// window.addEventListener('scroll', ()=> {
-//     document.querySelector('nav').classList.toggle
-//     ('window-scroll', window.scrollY > 0)
-// })
-
-
-
-async function fetchAsText(url){
+async function fetchAsText(url) {
     return await (await fetch(url)).text();
 }
 
@@ -17,5 +10,12 @@ async function importFooter(targetDiv) {
     document.querySelector('#' + targetDiv).innerHTML = await fetchAsText("../html/footer.html");
 }
 
+async function importFloatingBtn(targetDiv) {
+    document.querySelector('#' + targetDiv).innerHTML = await fetchAsText("../html/floating_btn.html");
+}
+
+
+
 importNav('nav');
 importFooter('footer');
+importFloatingBtn('floatingBtn');
